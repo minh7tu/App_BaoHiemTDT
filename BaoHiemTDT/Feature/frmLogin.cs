bfc558c35f589b933b493ffd68f8f1d6c228dc98
code.ntm <minh7tu>
2021-10-03 22:55:29 +0700

    Khoi tao

---
+++ BaoHiemTDT/Feature/frmLogin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BaoHiemTDT.Config;
+
+namespace BaoHiemTDT.Feature
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ BaoHiemTDT/Program.cs
@@ -0,0 +1,22 @@
+﻿using BaoHiemTDT.Feature;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BaoHiemTDT
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmLogin());
+        }
+    }
+}
