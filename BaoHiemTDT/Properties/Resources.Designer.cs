3cb51a5091ec60ccd9b00c4ce84901d46ccce29f
minh7tu <thuyetminh230921@gmail.com>
2021-10-12 21:26:48 +0700

    khởi tạo all

--- BaoHiemTDT/Properties/Resources.Designer.cs
+++ BaoHiemTDT/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace BaoHiemTDT.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap cÔNG_TY_CỔ_PHẦN_BẢO_HIỂM {
-            get {
-                object obj = ResourceManager.GetObject("cÔNG TY CỔ PHẦN BẢO HIỂM", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
