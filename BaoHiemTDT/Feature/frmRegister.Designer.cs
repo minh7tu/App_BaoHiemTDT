d61f925da953413a3f422657a02411f71e01029e
Bui Tien Dai <buitiendai111@gmail.com>
2021-10-15 08:31:46 +0700

    dai

--- BaoHiemTDT/Feature/frmRegister.Designer.cs
+++ BaoHiemTDT/Feature/frmRegister.Designer.cs
@@ -36,14 +36,18 @@ namespace BaoHiemTDT.Feature
             this.lblRegisterMK = new System.Windows.Forms.Label();
             this.lblRepeatMK = new System.Windows.Forms.Label();
             this.lblSoDT = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.txtRegisMK = new System.Windows.Forms.TextBox();
+            this.txtRegisTK = new System.Windows.Forms.TextBox();
+            this.txtRegisMKa = new System.Windows.Forms.TextBox();
+            this.txtRegisSDT = new System.Windows.Forms.TextBox();
             this.btnDangKy = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btnExit = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRegisEmail = new System.Windows.Forms.TextBox();
+            this.ckbDieuKien = new System.Windows.Forms.CheckBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -52,10 +56,10 @@ namespace BaoHiemTDT.Feature
             // pictureBox1
             // 
             this.pictureBox1.Image = global::BaoHiemTDT.Properties.Resources.logo_login1;
-            this.pictureBox1.Location = new System.Drawing.Point(181, 69);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(241, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(223, 190);
+            this.pictureBox1.Size = new System.Drawing.Size(297, 234);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -64,10 +68,9 @@ namespace BaoHiemTDT.Feature
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(215, 218);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(286, 192);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(167, 25);
+            this.label1.Size = new System.Drawing.Size(213, 33);
             this.label1.TabIndex = 1;
             this.label1.Text = "Đăng kí tài khoản";
             // 
@@ -75,10 +78,9 @@ namespace BaoHiemTDT.Feature
             // 
             this.lblRegisterTK.AutoSize = true;
             this.lblRegisterTK.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRegisterTK.Location = new System.Drawing.Point(154, 276);
-            this.lblRegisterTK.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRegisterTK.Location = new System.Drawing.Point(199, 253);
             this.lblRegisterTK.Name = "lblRegisterTK";
-            this.lblRegisterTK.Size = new System.Drawing.Size(71, 16);
+            this.lblRegisterTK.Size = new System.Drawing.Size(83, 19);
             this.lblRegisterTK.TabIndex = 2;
             this.lblRegisterTK.Text = "Tài khoản :";
             // 
@@ -86,10 +88,9 @@ namespace BaoHiemTDT.Feature
             // 
             this.lblRegisterMK.AutoSize = true;
             this.lblRegisterMK.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRegisterMK.Location = new System.Drawing.Point(157, 297);
-            this.lblRegisterMK.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRegisterMK.Location = new System.Drawing.Point(199, 294);
             this.lblRegisterMK.Name = "lblRegisterMK";
-            this.lblRegisterMK.Size = new System.Drawing.Size(68, 16);
+            this.lblRegisterMK.Size = new System.Drawing.Size(80, 19);
             this.lblRegisterMK.TabIndex = 2;
             this.lblRegisterMK.Text = "Mật khẩu :";
             // 
@@ -97,10 +98,9 @@ namespace BaoHiemTDT.Feature
             // 
             this.lblRepeatMK.AutoSize = true;
             this.lblRepeatMK.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRepeatMK.Location = new System.Drawing.Point(108, 330);
-            this.lblRepeatMK.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRepeatMK.Location = new System.Drawing.Point(199, 336);
             this.lblRepeatMK.Name = "lblRepeatMK";
-            this.lblRepeatMK.Size = new System.Drawing.Size(117, 16);
+            this.lblRepeatMK.Size = new System.Drawing.Size(138, 19);
             this.lblRepeatMK.TabIndex = 2;
             this.lblRepeatMK.Text = "Nhập lại mật khẩu :";
             // 
@@ -108,53 +108,64 @@ namespace BaoHiemTDT.Feature
             // 
             this.lblSoDT.AutoSize = true;
             this.lblSoDT.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSoDT.Location = new System.Drawing.Point(141, 362);
-            this.lblSoDT.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSoDT.Location = new System.Drawing.Point(199, 380);
             this.lblSoDT.Name = "lblSoDT";
-            this.lblSoDT.Size = new System.Drawing.Size(84, 16);
+            this.lblSoDT.Size = new System.Drawing.Size(104, 19);
             this.lblSoDT.TabIndex = 2;
             this.lblSoDT.Text = "Số điện thoại:";
             // 
-            // textBox1
+            // txtRegisMK
             // 
-            this.textBox1.Location = new System.Drawing.Point(245, 296);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(159, 20);
-            this.textBox1.TabIndex = 3;
+            this.txtRegisMK.Location = new System.Drawing.Point(343, 294);
+            this.txtRegisMK.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtRegisMK.Name = "txtRegisMK";
+            this.txtRegisMK.Size = new System.Drawing.Size(211, 22);
+            this.txtRegisMK.TabIndex = 2;
+            this.txtRegisMK.Text = "Nhập mật khẩu";
+            this.txtRegisMK.Enter += new System.EventHandler(this.txtRegisMK_Enter);
+            this.txtRegisMK.Leave += new System.EventHandler(this.txtRegisMK_Leave);
             // 
-            // textBox2
+            // txtRegisTK
             // 
-            this.textBox2.Location = new System.Drawing.Point(245, 272);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(159, 20);
-            this.textBox2.TabIndex = 3;
+            this.txtRegisTK.Location = new System.Drawing.Point(343, 253);
+            this.txtRegisTK.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtRegisTK.Name = "txtRegisTK";
+            this.txtRegisTK.Size = new System.Drawing.Size(211, 22);
+            this.txtRegisTK.TabIndex = 1;
+            this.txtRegisTK.Text = "Nhập tài khoản";
+            this.txtRegisTK.Enter += new System.EventHandler(this.txtRegisTK_Enter);
+            this.txtRegisTK.Leave += new System.EventHandler(this.txtRegisTK_Leave);
             // 
-            // textBox4
+            // txtRegisMKa
             // 
-            this.textBox4.Location = new System.Drawing.Point(245, 329);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(159, 20);
-            this.textBox4.TabIndex = 3;
+            this.txtRegisMKa.Location = new System.Drawing.Point(343, 336);
+            this.txtRegisMKa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtRegisMKa.Name = "txtRegisMKa";
+            this.txtRegisMKa.Size = new System.Drawing.Size(211, 22);
+            this.txtRegisMKa.TabIndex = 3;
+            this.txtRegisMKa.Text = "Nhập lại mật khẩu";
+            this.txtRegisMKa.Enter += new System.EventHandler(this.txtRegisMKa_Enter);
+            this.txtRegisMKa.Leave += new System.EventHandler(this.txtRegisMKa_Leave);
             // 
-            // textBox6
+            // txtRegisSDT
             // 
-            this.textBox6.Location = new System.Drawing.Point(245, 362);
-            this.textBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(159, 20);
-            this.textBox6.TabIndex = 3;
+            this.txtRegisSDT.Location = new System.Drawing.Point(343, 380);
+            this.txtRegisSDT.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtRegisSDT.Name = "txtRegisSDT";
+            this.txtRegisSDT.Size = new System.Drawing.Size(211, 22);
+            this.txtRegisSDT.TabIndex = 4;
+            this.txtRegisSDT.Text = "Nhập số điện thoại";
+            this.txtRegisSDT.Enter += new System.EventHandler(this.txtRegisSDT_Enter);
+            this.txtRegisSDT.Leave += new System.EventHandler(this.txtRegisSDT_Leave);
             // 
             // btnDangKy
             // 
             this.btnDangKy.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDangKy.Location = new System.Drawing.Point(245, 409);
-            this.btnDangKy.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDangKy.Location = new System.Drawing.Point(311, 503);
+            this.btnDangKy.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnDangKy.Name = "btnDangKy";
-            this.btnDangKy.Size = new System.Drawing.Size(102, 41);
-            this.btnDangKy.TabIndex = 4;
+            this.btnDangKy.Size = new System.Drawing.Size(136, 50);
+            this.btnDangKy.TabIndex = 7;
             this.btnDangKy.Text = "Đăng ký";
             this.btnDangKy.UseVisualStyleBackColor = true;
             // 
@@ -165,45 +176,95 @@ namespace BaoHiemTDT.Feature
             this.panel1.Controls.Add(this.btnExit);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(600, 26);
+            this.panel1.Size = new System.Drawing.Size(800, 32);
             this.panel1.TabIndex = 5;
-            // 
-            // btnExit
-            // 
-            this.btnExit.FlatAppearance.BorderSize = 0;
-            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
-            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExit.Location = new System.Drawing.Point(562, 0);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(38, 26);
-            this.btnExit.TabIndex = 0;
-            this.btnExit.Text = "x";
-            this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // pictureBox2
             // 
             this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Left;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(32, 26);
+            this.pictureBox2.Size = new System.Drawing.Size(43, 32);
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
+            // btnExit
+            // 
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Location = new System.Drawing.Point(749, 0);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(51, 32);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "x";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(199, 429);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Email :";
+            // 
+            // txtRegisEmail
+            // 
+            this.txtRegisEmail.Location = new System.Drawing.Point(343, 429);
+            this.txtRegisEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtRegisEmail.Name = "txtRegisEmail";
+            this.txtRegisEmail.Size = new System.Drawing.Size(211, 22);
+            this.txtRegisEmail.TabIndex = 5;
+            this.txtRegisEmail.Text = "Nhập vào Email";
+            this.txtRegisEmail.Enter += new System.EventHandler(this.txtRegisEmail_Enter);
+            this.txtRegisEmail.Leave += new System.EventHandler(this.txtRegisEmail_Leave);
+            // 
+            // ckbDieuKien
+            // 
+            this.ckbDieuKien.AutoSize = true;
+            this.ckbDieuKien.Location = new System.Drawing.Point(241, 477);
+            this.ckbDieuKien.Name = "ckbDieuKien";
+            this.ckbDieuKien.Size = new System.Drawing.Size(289, 21);
+            this.ckbDieuKien.TabIndex = 6;
+            this.ckbDieuKien.Text = "Tôi đã đọc và đồng ý với các điều khoản.";
+            this.ckbDieuKien.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(357, 575);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(51, 20);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Thoát";
+            // 
             // frmRegister
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(239)))));
-            this.ClientSize = new System.Drawing.Size(600, 461);
+            this.ClientSize = new System.Drawing.Size(800, 601);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.ckbDieuKien);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnDangKy);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtRegisEmail);
+            this.Controls.Add(this.txtRegisSDT);
+            this.Controls.Add(this.txtRegisMKa);
+            this.Controls.Add(this.txtRegisTK);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRegisMK);
             this.Controls.Add(this.lblSoDT);
             this.Controls.Add(this.lblRepeatMK);
             this.Controls.Add(this.lblRegisterMK);
@@ -211,7 +272,7 @@ namespace BaoHiemTDT.Feature
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmRegister";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmRegister";
@@ -232,13 +293,17 @@ namespace BaoHiemTDT.Feature
         private System.Windows.Forms.Label lblRegisterMK;
         private System.Windows.Forms.Label lblRepeatMK;
         private System.Windows.Forms.Label lblSoDT;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox txtRegisMK;
+        private System.Windows.Forms.TextBox txtRegisTK;
+        private System.Windows.Forms.TextBox txtRegisMKa;
+        private System.Windows.Forms.TextBox txtRegisSDT;
         private System.Windows.Forms.Button btnDangKy;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRegisEmail;
+        private System.Windows.Forms.CheckBox ckbDieuKien;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
--- BaoHiemTDT/Feature/frmRegister.cs
+++ BaoHiemTDT/Feature/frmRegister.cs
@@ -15,7 +15,7 @@ namespace BaoHiemTDT.Feature
         {
             InitializeComponent();
         }
-
+        //Placeholder cho textbox
         private void frmRegister_Load(object sender, EventArgs e)
         {
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -26,6 +26,104 @@ namespace BaoHiemTDT.Feature
             Application.Exit();
         }
 
+        private void txtRegisTK_Enter(object sender, EventArgs e)
+        {
+            if (txtRegisTK.Text == "Nhập tài khoản")
+            {
+                txtRegisTK.Text = "";
 
+                txtRegisTK.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtRegisMK_Enter(object sender, EventArgs e)
+        {
+            if (txtRegisMK.Text == "Nhập mật khẩu")
+            {
+                txtRegisMK.Text = "";
+
+                txtRegisMK.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtRegisMKa_Enter(object sender, EventArgs e)
+        {
+            if (txtRegisMKa.Text == "Nhập lại mật khẩu")
+            {
+                txtRegisMKa.Text = "";
+
+                txtRegisMKa.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtRegisSDT_Enter(object sender, EventArgs e)
+        {
+            if (txtRegisSDT.Text == "Nhập số điện thoại")
+            {
+                txtRegisSDT.Text = "";
+
+                txtRegisSDT.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtRegisEmail_Enter(object sender, EventArgs e)
+        {
+            if (txtRegisEmail.Text == "Nhập vào Email")
+            {
+                txtRegisEmail.Text = "";
+
+                txtRegisEmail.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtRegisTK_Leave(object sender, EventArgs e)
+        {
+            if (txtRegisTK.Text == "")
+            {
+                txtRegisTK.Text = "Nhập tài khoản";
+
+                txtRegisTK.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtRegisMK_Leave(object sender, EventArgs e)
+        {
+            if (txtRegisMK.Text == "")
+            {
+                txtRegisMK.Text = "Nhập mật khẩu";
+
+                txtRegisMK.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtRegisMKa_Leave(object sender, EventArgs e)
+        {
+            if (txtRegisMKa.Text == "")
+            {
+                txtRegisMKa.Text = "Nhập vào mật khẩu";
+
+                txtRegisMKa.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtRegisSDT_Leave(object sender, EventArgs e)
+        {
+            if (txtRegisSDT.Text == "")
+            {
+                txtRegisSDT.Text = "Nhập số điện thoại";
+
+                txtRegisSDT.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtRegisEmail_Leave(object sender, EventArgs e)
+        {
+            if (txtRegisEmail.Text == "")
+            {
+                txtRegisEmail.Text = "Nhập vào Email";
+
+                txtRegisEmail.ForeColor = Color.Silver;
+            }
+        }
     }
 }
