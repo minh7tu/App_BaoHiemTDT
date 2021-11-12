f3a60ee0b86e4bdbacb491098b38d23e33a5b2e5
minh7tu <thuyetminh230921@gmail.com>
2021-11-12 19:12:48 +0700

    sửa path

---
+++ BaoHiemTDT/Config/TDT.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace BaoHiemTDT.Config
+{
+    class TDT
+    {
+        public static SqlConnection connect;
+
+        public TDT()
+        { 
+        
+        }
+
+        public void Connect()
+        {
+            string str = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\Data\BaoHiemTDT.mdf;Integrated Security=True";
+            connect = new SqlConnection(str);
+            connect.Open();
+        }
+
+        public void Close()
+        {
+            if (connect.State == System.Data.ConnectionState.Open)
+            {
+                connect.Close();
+                connect.Dispose();//Giải phóng tài nguyên
+                connect = null;
+            }
+        }
+    }
+}
