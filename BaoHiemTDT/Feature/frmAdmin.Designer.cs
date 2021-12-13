3af4e58f4434c0de302ee9bc6ab75126f7648d33
Đại <buitiendai111@gmail.com>
2021-12-13 17:21:07 +0700

    Hoan thien admin

--- BaoHiemTDT/Feature/frmAdmin.Designer.cs
+++ BaoHiemTDT/Feature/frmAdmin.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BaoHiemTDT.Feature
 {
-    partial class Form1
+    partial class frmAdmin
     {
         /// <summary>
         /// Required designer variable.
@@ -28,96 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.btnLuu = new System.Windows.Forms.Button();
-            this.btnSua = new System.Windows.Forms.Button();
+            this.grbQuanLy = new System.Windows.Forms.GroupBox();
             this.dgvQuanLy = new System.Windows.Forms.DataGridView();
-            this.groupBox1.SuspendLayout();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.HoTen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SDT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GioiTinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NgaySinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DiaChi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grbQuanLy.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvQuanLy)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // grbQuanLy
             // 
-            this.groupBox1.Controls.Add(this.dgvQuanLy);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(0, 0);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(857, 509);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Quản lý tài khoản";
+            this.grbQuanLy.Controls.Add(this.dgvQuanLy);
+            this.grbQuanLy.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grbQuanLy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grbQuanLy.Location = new System.Drawing.Point(0, 0);
+            this.grbQuanLy.Name = "grbQuanLy";
+            this.grbQuanLy.Size = new System.Drawing.Size(1072, 560);
+            this.grbQuanLy.TabIndex = 0;
+            this.grbQuanLy.TabStop = false;
+            this.grbQuanLy.Text = "Quản lý tài khoản";
+            // 
+            // dgvQuanLy
+            // 
+            this.dgvQuanLy.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvQuanLy.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvQuanLy.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.HoTen,
+            this.SDT,
+            this.GioiTinh,
+            this.NgaySinh,
+            this.DiaChi,
+            this.Email});
+            this.dgvQuanLy.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvQuanLy.Location = new System.Drawing.Point(3, 26);
+            this.dgvQuanLy.Name = "dgvQuanLy";
+            this.dgvQuanLy.RowTemplate.Height = 24;
+            this.dgvQuanLy.Size = new System.Drawing.Size(1066, 531);
+            this.dgvQuanLy.TabIndex = 0;
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Controls.Add(this.btnLuu, 1, 0);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.btnSua, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnLuu, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 479);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 529);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(857, 30);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1072, 31);
             this.tableLayoutPanel1.TabIndex = 1;
-            // 
-            // btnLuu
-            // 
-            this.btnLuu.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnLuu.Location = new System.Drawing.Point(431, 3);
-            this.btnLuu.Name = "btnLuu";
-            this.btnLuu.Size = new System.Drawing.Size(423, 24);
-            this.btnLuu.TabIndex = 1;
-            this.btnLuu.Text = "Lưu";
-            this.btnLuu.UseVisualStyleBackColor = true;
             // 
             // btnSua
             // 
             this.btnSua.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnSua.Location = new System.Drawing.Point(3, 3);
             this.btnSua.Name = "btnSua";
-            this.btnSua.Size = new System.Drawing.Size(422, 24);
-            this.btnSua.TabIndex = 2;
+            this.btnSua.Size = new System.Drawing.Size(530, 25);
+            this.btnSua.TabIndex = 0;
             this.btnSua.Text = "Sửa";
             this.btnSua.UseVisualStyleBackColor = true;
             // 
-            // dgvQuanLy
+            // btnLuu
             // 
-            this.dgvQuanLy.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvQuanLy.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvQuanLy.Location = new System.Drawing.Point(3, 26);
-            this.dgvQuanLy.Name = "dgvQuanLy";
-            this.dgvQuanLy.RowTemplate.Height = 24;
-            this.dgvQuanLy.Size = new System.Drawing.Size(851, 480);
-            this.dgvQuanLy.TabIndex = 0;
+            this.btnLuu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnLuu.Location = new System.Drawing.Point(539, 3);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(530, 25);
+            this.btnLuu.TabIndex = 1;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // HoTen
+            // 
+            this.HoTen.DataPropertyName = "HoTen";
+            this.HoTen.FillWeight = 133.8923F;
+            this.HoTen.HeaderText = "Họ Tên";
+            this.HoTen.Name = "HoTen";
+            // 
+            // SDT
+            // 
+            this.SDT.DataPropertyName = "SDT";
+            this.SDT.FillWeight = 105.1054F;
+            this.SDT.HeaderText = "Số điện thoại";
+            this.SDT.Name = "SDT";
+            // 
+            // GioiTinh
+            // 
+            this.GioiTinh.DataPropertyName = "GioiTinh";
+            this.GioiTinh.FillWeight = 45.68523F;
+            this.GioiTinh.HeaderText = "Giới Tính";
+            this.GioiTinh.Name = "GioiTinh";
+            // 
+            // NgaySinh
+            // 
+            this.NgaySinh.DataPropertyName = "NgaySinh";
+            this.NgaySinh.FillWeight = 105.1054F;
+            this.NgaySinh.HeaderText = "Ngày Sinh";
+            this.NgaySinh.Name = "NgaySinh";
+            // 
+            // DiaChi
+            // 
+            this.DiaChi.DataPropertyName = "DiaChi";
+            this.DiaChi.FillWeight = 105.1054F;
+            this.DiaChi.HeaderText = "Địa Chỉ";
+            this.DiaChi.Name = "DiaChi";
+            // 
+            // Email
+            // 
+            this.Email.DataPropertyName = "Email";
+            this.Email.FillWeight = 105.1054F;
+            this.Email.HeaderText = "Email";
+            this.Email.Name = "Email";
+            // 
+            // frmAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(857, 509);
+            this.ClientSize = new System.Drawing.Size(1072, 560);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Controls.Add(this.groupBox1);
-            this.Name = "Form1";
+            this.Controls.Add(this.grbQuanLy);
+            this.Name = "frmAdmin";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.frmAdmin_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.tableLayoutPanel1.ResumeLayout(false);
+            this.grbQuanLy.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvQuanLy)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox grbQuanLy;
         private System.Windows.Forms.DataGridView dgvQuanLy;
-        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HoTen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SDT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GioiTinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NgaySinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DiaChi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
     }
 }
--- BaoHiemTDT/Feature/frmAdmin.cs
+++ BaoHiemTDT/Feature/frmAdmin.cs
@@ -10,28 +10,24 @@ using System.Data.SqlClient;
 
 namespace BaoHiemTDT.Feature
 {
-    public partial class Form1 : Form
+    public partial class frmAdmin : Form
     {
-        public Form1()
+        public frmAdmin()
         {
             InitializeComponent();
         }
 
+      
         private void frmAdmin_Load(object sender, EventArgs e)
         {
             BaoHiemTDT.Config.TDT.Connect();
-            /*SqlConnection con = new SqlConnection();
-            con.ConnectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=E:\BaoHiem\BaoHiemTDT\BaoHiemTDT\Data\BaoHiemTDT.mdf;Integrated Security=True";
-            con.Open();*/
-            string sql = "Select * from KHACHHANG";
+            String sql = "Select * from KHACHHANG";
             DataSet ds = new DataSet();
-            SqlDataAdapter dap = new SqlDataAdapter(sql,BaoHiemTDT.Config.TDT.connect);
-            dap.Fill(ds);
-
-            //Gan du lieu len Datagrid
+            SqlDataAdapter dap = new SqlDataAdapter(sql, BaoHiemTDT.Config.TDT.connect);
+            dap.Fill(ds); 
+            //Gan du lieu len dgv
             dgvQuanLy.DataSource = ds.Tables[0];
             dgvQuanLy.Refresh();
-
         }
     }
 }
